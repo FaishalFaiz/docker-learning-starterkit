d3b8ea55c1dc06dee78fd8dd8d6c3eeaa99d0cc8
faishalfn <faishalnugrohofaiz@gmail.com>
2026-07-20 09:55:53 +0700

    bab 2 & 3

---
+++ bab-3/declaration.dart
@@ -0,0 +1,9 @@
+void main() {
+  var alamat = 'Yogyakarta Indonesia';
+  final String = '231232310412';
+  const double height = 1.75;
+
+  print('Hello, World! alamatku adalah $alamat');
+  print('NIMku adalah $String');
+  print('Tinggi badanku adalah $height meter');
+}
---
+++ bab-3/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = 'Faiz Faishal Nugroho';
+  int age = 20;
+  double height = 1.75;
+  bool isStudent = true;
+
+  print('Hello, World! Namaku adalah $name');
+  print('Umurku adalah $age tahun');
+  print('Tinggi badanku adalah $height meter');
+  print('Aku adalah seorang mahasiswa: $isStudent');
+}
